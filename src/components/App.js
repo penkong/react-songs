456a2b1e1a1dc344f36e20a4dcc4edc3f9967136
mazdak <nazemi.works@gmail.com>
2019-05-16 06:39:41 -0700

    app done with redux and actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 //actions come in comps for take info and send back to store mama.
 
 
 const App = () => {
   return (
-    <div className="ui container">
-      <SongList/>
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList/>
+        </div>
+        <div className="column eight wide">
+          <SongDetail/>
+        </div>
+      </div>
     </div>
   );
 };
